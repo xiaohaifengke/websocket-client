a2e7856e236d4a3d57de2b82e77000f44f8b1a02
Jhail <Jhail@foxmail.com>
2022-01-25 15:28:57 +0800

    build: 架构重构

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,155 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module'
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module"
   },
   env: {
     es6: true,
     node: true,
     browser: true
   },
-  parser: 'babel-eslint',
-  extends: ['eslint:recommended', 'prettier'],
-  // plugins: ["prettier"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
   globals: {},
   rules: {
-    'no-console': 0,
-    'spaced-comment': 2,
-    'no-multiple-empty-lines': 2,
-    'multiline-comment-style': 2,
-    'prefer-const': 2,
-    'lines-around-comment': 2,
-    'space-before-function-paren': 2,
-    'eqeqeq': 2
+    // "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "accessor-pairs": 2,
+    "camelcase": [
+      0,
+      {
+        properties: "always",
+      }
+    ],
+    "constructor-super": 2,
+    "eqeqeq": ["error", "always", { null: "ignore" }],
+    "handle-callback-err": [2, "^(err|error)$"],
+    "new-cap": [
+      2,
+      {
+        newIsCap: true,
+        capIsNew: false
+      }
+    ],
+    "no-array-constructor": 2,
+    "no-caller": 2,
+    "no-class-assign": 2,
+    "no-cond-assign": 2,
+    "no-const-assign": 2,
+    "no-control-regex": 0,
+    "no-delete-var": 2,
+    "no-dupe-args": 2,
+    "no-dupe-class-members": 2,
+    "no-dupe-keys": 2,
+    "no-duplicate-case": 2,
+    "no-empty-character-class": 2,
+    "no-empty-pattern": 2,
+    "no-eval": 2,
+    "no-ex-assign": 2,
+    "no-extend-native": 2,
+    "no-extra-bind": 2,
+    "no-extra-boolean-cast": 2,
+    "no-fallthrough": 2,
+    "no-func-assign": 2,
+    "no-implied-eval": 2,
+    "no-inner-declarations": [2, "functions"],
+    "no-invalid-regexp": 2,
+    "no-irregular-whitespace": 2,
+    "no-iterator": 2,
+    "no-label-var": 2,
+    "no-labels": [
+      2,
+      {
+        allowLoop: false,
+        allowSwitch: false
+      }
+    ],
+    "no-lone-blocks": 2,
+    "no-multi-str": 2,
+    "no-native-reassign": 2,
+    "no-negated-in-lhs": 2,
+    "no-new-object": 2,
+    "no-new-require": 2,
+    "no-new-symbol": 2,
+    "no-new-wrappers": 2,
+    "no-obj-calls": 2,
+    "no-octal": 2,
+    "no-octal-escape": 2,
+    "no-path-concat": 2,
+    "no-proto": 2,
+    "no-redeclare": 2,
+    "no-regex-spaces": 2,
+    "no-return-assign": [2, "except-parens"],
+    "no-self-assign": 2,
+    "no-self-compare": 2,
+    "no-sequences": 2,
+    "no-shadow-restricted-names": 2,
+    "no-sparse-arrays": 2,
+    "no-this-before-super": 2,
+    "no-throw-literal": 2,
+    "no-undef": 2,
+    "no-undef-init": 2,
+    "no-unexpected-multiline": 2,
+    "no-unmodified-loop-condition": 2,
+    "no-unneeded-ternary": [
+      2,
+      {
+        defaultAssignment: false
+      }
+    ],
+    "no-unreachable": 2,
+    "no-unsafe-finally": 2,
+    "no-unused-vars": [
+      2,
+      {
+        vars: "all",
+        args: "none",
+      }
+    ],
+    "no-useless-call": 2,
+    "no-useless-computed-key": 2,
+    "no-useless-constructor": 2,
+    "no-useless-escape": 0,
+    "no-with": 2,
+    "one-var": [
+      2,
+      {
+        initialized: "never",
+      }
+    ],
+    "quotes": [
+      2,
+      "single",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    "space-before-function-paren": [0, "never"],
+    "spaced-comment": [
+      2,
+      "always",
+      {
+        markers: [
+          "global",
+          "globals",
+          "eslint",
+          "eslint-disable",
+          "*package",
+          "!",
+          ",",
+        ]
+      }
+    ],
+    "use-isnan": 2,
+    "valid-typeof": 2,
+    "yoda": [2, "never"],
+    "prefer-const": 2
   }
 }
---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 0.5.6 (2022-01-25)
+
+
+### Bug Fixes
+
+* **readme.md:** fixed a comment problem ([8f26936](https://github.com/xiaohaifengke/websocket-client/commit/8f2693600cce5364f15e64196c611f3781d61357))
+
+
+
